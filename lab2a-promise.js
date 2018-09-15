2197f3689f0f712973b1213ef51528266f97ce16
Medellin Qin <Medellin@github.com>
2018-09-15 14:10:18 -0400

    Part A done!!

--- lab2a-promise.js
+++ lab2a-promise.js
@@ -1,5 +1,4 @@
 ///// Problem 1 - Fun with Promises
-
 function flip() {
     let p = new Promise(function (resolve, reject) {
         if (Math.random() > 0.5) resolve();
@@ -8,23 +7,31 @@ function flip() {
     return p;
 }
 
-//// Add code here that will "flip" the coin ten times and write the 
+//// Add code here that will "flip" the coin ten times and write the
 //// result to the console (e.g. "Heads" or "Tails" for each flip).
-
-
-
+let p = flip();
+for (let i = 0; i < 10; i++) {
+    p.then(
+      () => {console.log("Heads!");},
+      () => {console.log("Tails!");}
+    );
+    p = flip();
+}
 
 ///// Problem 2 - More fun...
 
 function countBig(bignum) {
     // Add code here that returns a Promise that will resolve after it has counted to bignum
+    return new Promise((resolve, reject) => {
+      for(let i = 0;i<=bignum;i++){
+        if(i == bignum) resolve();
+        if(i > bignum) reject();
+      }
+    });
 }
-
-
 start = Date.now();
 bignum = 1000000000;
-countBig(1000000000).then(()=> {
-    console.log("It took " + (Date.now() - start) + " ms to count to " + bignum);
-}, () => {
-    console.log("A problem occurred while trying to count to " + bignum);
-})
+countBig(bignum).then(
+  ()=> {console.log("It took " + (Date.now() - start) + " ms to count to " + bignum);},
+  () => {console.log("A problem occurred while trying to count to " + bignum);}
+)
